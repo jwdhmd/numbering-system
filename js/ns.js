b77056fdc652dca0f9014617632a40afc43e12a1
Jawad Ahmed <ahmed.jawad@ymail.com>
2013-05-24 20:59:18 +0530

    Displaying all numbers from 1 to n

--- js/ns.js
+++ js/ns.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	$("#run").click(function(){
-		alert($("#number").val())
+		$("#display li").remove();
+		var number = $("#number").val();
+		for (var i = 1; i <= number; i++) {
+			$("#display").append("<li>" + i + "</li>");
+		}
 	});
 });
